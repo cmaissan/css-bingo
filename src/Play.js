567e8979c24b44fe39dafbdc7d307e1e6b56ad76
Chris Maissan <chris@maissan.net>
2022-10-20 10:37:27 -0400

    Randomly show selectors one at a time

--- src/Play.js
+++ src/Play.js
@@ -1,8 +1,44 @@
 import './Play.css';
+import data from './data.json';
+import { useEffect, useState } from 'react';
+import Selector from './Selector';
 
 function Play() {
+
+  const [ selectors, setSelectors ] = useState([]);
+  const [ currentSelector, setCurrentSelector ] = useState(-1);
+  const [ isFinished, setIsFinished ] = useState(false);
+
+  // Shuffle selectors
+  useEffect(() => {
+    let list = data.B.concat(data.I).concat(data.N).concat(data.G).concat(data.O);
+    list = list.sort((a, b) => 0.5 - Math.random());
+    setSelectors(list);
+    setCurrentSelector(0);
+  }, []);
+
+  // Show next selector
+  const next = () => {
+    if (currentSelector < selectors.length - 1) {
+      setCurrentSelector(currentSelector + 1);
+    } else {
+      setIsFinished(true);
+    }
+  };
+
+  // Show selector
   return (
     <div className="Play">
+
+      {isFinished && <>
+        <div className="Play-Finished"></div>
+      </>}
+
+      {!isFinished && <>
+        {currentSelector > -1 && <Selector markup={selectors[currentSelector].markup} />}
+        <button onClick={next}>Next</button>
+      </>}
+
     </div>
   );
 }
---
+++ src/Selector.js
@@ -0,0 +1,11 @@
+import './Selector.css';
+
+function Selector(props) {
+  return (
+    <div className="Selector">
+      {props.markup}
+    </div>
+  );
+}
+
+export default Selector;
